7f1301246e3725f4346e755cc4f73ced6a5d19e2
George Roehsner <44953382+Groehsner@users.noreply.github.com>
2018-11-12 00:53:19 -0500

    Add files via upload

---
+++ GetCredentials.ps1
@@ -0,0 +1,60 @@
+﻿### This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+### Author: George Roehsner
+
+# use this in scripts needing credentials. update the source and destination systems as necessary
+
+# where to read file of encrypted credentials
+# this script will use the credentials retrieved from here
+$CredentialsFile = "C:\automation\scripts\sources\automationdata.xml"
+
+# update these for the script we're in
+$SourceSystem = "Ultradata"
+$DestinationSystem = "Vintek"
+
+# read existing file
+try {
+    # file was saved with export-clixml so import it if it exists
+    $ListOfCredentials = Import-Clixml $CredentialsFile -ErrorAction Stop
+} catch {
+    # if file is not found then you can send it to log or let transcript capture it if running
+    # exit script because without login credentials you can't do anything
+    Write-Host "Error reading credentials file"
+    Stop-Transcript
+    exit
+}
+
+# read the source system credentials into a username and password variable.
+# get each value and convert it to a secure string
+try {
+    $SourceUsernameEncrypted = $ListOfCredentials.$SourceSystem.Username | ConvertTo-SecureString -ErrorAction Stop
+    # now to get unencrypted username and password. 
+    # copy secure string into unencrypted memory location and get a pointer for where the data is stored
+    $SourceUsernameBinaryStringPointer = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SourceUsernameEncrypted)
+
+    # read the data from memory and store in variable
+    $SourceUsername = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($SourceUsernameBinaryStringPointer)
+
+    # free the memory used by this process
+    [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($SourceUsernameBinaryStringPointer)
+
+    # same steps but for password
+    $SourcePasswordEncrypted = $ListOfCredentials.$SourceSystem.Password | ConvertTo-SecureString
+    $SourcePasswordBinaryStringPointer = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SourcePasswordEncrypted)
+    $SourcePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($SourcePasswordBinaryStringPointer)
+    [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($SourcePasswordBinaryStringPointer)
+
+    # same as for source but for destination
+    $DestinationUsernameEncrypted = $ListOfCredentials.$DestinationSystem.Username | ConvertTo-SecureString
+    $DestinationUsernameBinaryStringPointer = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($DestinationUsernameEncrypted)
+    $DestinationUsername = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($DestinationUsernameBinaryStringPointer)
+    [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($DestinationUsernameBinaryStringPointer)
+    $DestinationPasswordEncrypted = $ListOfCredentials.$DestinationSystem.Password | ConvertTo-SecureString
+    $DestinationPasswordBinaryStringPointer = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($DestinationPasswordEncrypted)
+    $DestinationPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($DestinationPasswordBinaryStringPointer)
+    [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($DestinationPasswordBinaryStringPointer)
+} catch {
+    Write-Host "Error reading encrypted string. Probably because a different user or machine is trying to read this."
+    Stop-Transcript
+    exit
+}
+
---
+++ ManageCredentials.ps1
@@ -0,0 +1,64 @@
+﻿### This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+### Author: George Roehsner
+
+# This script will prompt a user for systems and the credentials for them. It will save those credentials in a file spcecified in this script.
+# The credentials, both username and password, are encrypted using windows dpapi features.
+Start-Transcript -Path "c:\automation\logs\ManageCredentials$(get-date -format FileDateTime).log" -Append
+
+# Update where to save file of encrypted credentials
+$CredentialsFile = "C:\automation\scripts\sources\automationdata.xml"
+
+# read existing file
+try {
+    # file was saved with export-clixml so import it if it exists
+    $ListOfCredentials = Import-Clixml $CredentialsFile -ErrorAction Stop
+} catch {
+    # if file is not found then create new dictionary object
+    Write-Host "Can't read existing file. Starting new file of credentials"
+    $ListOfCredentials = @{}
+}
+
+Write-Host "Add new or update existing credentials. Start by entering a name to identify what the credentials are for. Then enter the username, then password."
+# loop through reading credential objects until q or quit is entered
+:MainLoop While ($True) {
+    $CredentialsFor = Read-Host -Prompt "Enter Name of system. s or save to write file, q or quit to exit"
+    switch -regex ($CredentialsFor) {
+        '^q(uit)?$' {
+            # user chose to quit script. exit this while loop, then finish up any necessary script cleanup.
+            Write-Host "Exiting the script"
+            break MainLoop
+        }
+        '^s(ave)?$' {
+            # user chose to save. save file, catching and reporting errors, then start main loop over.
+            Write-Host "Saving file"
+            try {
+                # save hashtable object as xml file
+                $ListOfCredentials | Export-Clixml $CredentialsFile -ErrorAction Stop
+            } catch {
+                # there was an error writing file, notify user and then end script
+                Write-Host "There was an error writing the file:" -ForegroundColor Red
+                Write-Host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
+                Write-Host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
+                
+                # allow user to review error message. Wait for them to return before ending script
+                Read-Host -Prompt "Nothing was saved. Ending the script now. Return when ready."
+                break MainLoop
+            }
+            # if we're here then the file was saved without error.
+            Write-Host "Credentials saved to file."
+        }
+
+        default {
+            # user started a credential creation/update process. Get username and password now.
+            # prompt user for the information, retrieving it as an encrypted value (AsSecureString)
+            # then converting it to an actual string, still encrypted, so we can save it in a file.
+            $CredentialsUsername = Read-Host -Prompt "Username for system" -AsSecureString | ConvertFrom-SecureString
+            $CredentialsPassword = Read-Host -Prompt "Password for system" -AsSecureString | ConvertFrom-SecureString
+
+            # now that we have all of the information. Add this credential to the list
+            $ListOfCredentials.$CredentialsFor = @{"Username"=$CredentialsUsername;"Password"=$CredentialsPassword}
+        }
+    }
+
+}
+Stop-Transcript
